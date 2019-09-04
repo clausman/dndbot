47cc95d9828427b2a368669bae63746d5bc9e717
Josh Clausman <joclausm@microsoft.com>
2019-09-04 12:57:12 -0400

    switch to using serverless

--- handler.spec.ts
+++ handler.spec.ts
@@ -1,17 +1,113 @@
 import 'mocha';
 import * as chai from 'chai';
-// import { APIGatewayEvent, Handler, Callback, Context } from 'aws-lambda';
-import { hello } from './handler';
+import * as sinon from 'sinon';
+import { handleSlackCommand, roll } from './handler';
+import * as slackHelpers from './slackHelpers';
+import { APIGatewayProxyEvent } from 'aws-lambda';
 
 const expect = chai.expect;
 
 describe("handler", () => {
-  describe("hello", () => {
-    it("should return Serverless boilerplate message", () => {
-      hello(null, null, (error : Error, result : any) => {
-        expect(error).to.be.null;
-        result.body.should.equal('{"message":"Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!","input":null}');
-      })
+  describe("roll", () => {
+    it("handles simple roll", async () => {
+      const fakeEvent = {
+        queryStringParameters: { text: '1d1+10' },
+      } as unknown as APIGatewayProxyEvent;
+      roll(fakeEvent, null, (error, response) => {
+        expect(error).to.be.undefined;
+        expect(response.statusCode).to.eq(200);
+        expect(response.body).to.be('11 - [1]');
+      });
     });
   });
+
+  describe("slash command", () => {
+    let isValidTokenStub: sinon.SinonStub<[string, string], boolean> = undefined;
+
+    const testSlashCommand: slackHelpers.SlashCommand = {
+      token: 'token',
+      user_id: 'tester_id',
+      user_name: 'tester',
+      response_url: 'response_url',
+      trigger_id: 'trigger_id',
+      command: '/unsupported'
+    }
+
+    before(() => {
+      isValidTokenStub = sinon.stub(slackHelpers, 'isValidToken');
+    });
+
+    after(() => {
+      isValidTokenStub.restore();
+    });
+
+    describe("/roll", () => {
+      const validRollCommand: slackHelpers.SlashCommand = {
+        ...testSlashCommand,
+        command: '/roll',
+        text: '1d1+2'
+      }
+      
+      it("should not accept empty text", async () => {
+        isValidTokenStub.returns(true);
+        const result = await handleSlackCommand({
+          ...validRollCommand,
+          text: ''
+        });
+        expect(result.statusCode).to.eq(200);
+        const responseMessage = JSON.parse(result.body);
+        expect(responseMessage.text).to.contain('Roll text required');
+      });
+
+      it("should respond for a valid roll", async () => {
+        isValidTokenStub.returns(true);
+        const result = await handleSlackCommand(validRollCommand);
+        expect(result.statusCode).to.eq(200);
+        const responseMessage = JSON.parse(result.body);
+        expect(responseMessage.text).to.eq('3 - [1]');
+        expect(responseMessage.response_type).to.eq('in_channel');
+      });
+
+      it('should return unauthorized on bad access tokens', async () => {
+        isValidTokenStub.returns(false);
+        const result = await handleSlackCommand(validRollCommand);
+        expect(result.statusCode).to.eq(401);
+      });
+    });
+
+    // This is copy-paste from /roll except that in_channel is different.
+    describe("/sroll", () => {
+      const validSecretRollCommand: slackHelpers.SlashCommand = {
+        ...testSlashCommand,
+        command: '/sroll',
+        text: '1d1+2'
+      }
+      
+      it("should not accept empty text", async () => {
+        isValidTokenStub.returns(true);
+        const result = await handleSlackCommand({
+          ...validSecretRollCommand,
+          text: ''
+        });
+        expect(result.statusCode).to.eq(200);
+        const responseMessage = JSON.parse(result.body);
+        expect(responseMessage.text).to.contain('Roll text required');
+      });
+
+      it("should respond for a valid roll", async () => {
+        isValidTokenStub.returns(true);
+        const result = await handleSlackCommand(validSecretRollCommand);
+        expect(result.statusCode).to.eq(200);
+        const responseMessage = JSON.parse(result.body);
+        expect(responseMessage.text).to.eq('3 - [1]');
+        expect(responseMessage.response_type).to.be.undefined;
+      });
+
+      it('should return unauthorized on bad access tokens', async () => {
+        isValidTokenStub.returns(false);
+        const result = await handleSlackCommand(validSecretRollCommand);
+        expect(result.statusCode).to.eq(401);
+      });
+    });
+  })
 });
--- handler.ts
+++ handler.ts
@@ -1,12 +1,95 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
-import 'source-map-support/register';
+import { APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda';
+import * as qs from 'querystring';
+import * as Roll from 'roll';
 
-export const hello: APIGatewayProxyHandler = async (event, _context) => {
+import 'source-map-support/register';
+import { isValidToken, isSlashCommand, SlashCommand } from './slackHelpers';
+
+/* Endpoint to easily test roll logic */
+export const roll: APIGatewayProxyHandler = async (event, _context) => {
+  if (!(event.queryStringParameters && event.queryStringParameters.text)) {
+    return {
+      statusCode: 400,
+      body: "Invalid request. Query parameter 'text' is required."
+    };
+  }
+  const rollText = event.queryStringParameters.text;
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
+      message: handleRoll(rollText),
+      text: rollText,
     }, null, 2),
   };
+}
+
+/* Endpoint to handle slack webhook */
+export const webhook: APIGatewayProxyHandler = async (event, _context) => {
+  const payload = qs.parse(event.body);
+  if (!isSlashCommand(payload)) {
+    return {
+      statusCode: 400,
+      body: 'Invalid message format.'
+    };
+  }
+
+  return handleSlackCommand(payload);  
+}
+
+const rollCommandOptionsMap = {
+  '/roll': { inChannel: true, token: process.env.SLACK_TOKEN_ROLL_COMMAND },
+  '/sroll': { inChannel: false, token: process.env.SLACK_TOKEN_SROLL_COMMAND }
+};
+
+export async function handleSlackCommand(payload: SlashCommand) {
+  const rollCommandOptions = rollCommandOptionsMap[payload.command];
+  if (!rollCommandOptions) {
+    return slackMessageResult({
+      text: 'Unsupported slash command. Only /roll and /sroll are supported.'
+    });
+  }
+
+  if (!isValidToken(payload.token, rollCommandOptions.token)) {
+    return {statusCode: 401, body: 'Unauthorized.'};
+  }
+
+  if (!payload.text) {
+    return slackMessageResult({text: 'Roll text required. e.g. /roll 3d4+7'});
+  }
+
+  const rollText = payload.text.replace(/^[/]/, '');
+
+  return slackMessageResult({
+    text: handleRoll(rollText)
+  }, rollCommandOptions.inChannel);
+}
+
+function slackMessageResult(message: {text: string}, inChannel: boolean = false): APIGatewayProxyResult {
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      ...message,
+      response_type: inChannel ? 'in_channel' : undefined
+    })
+  };
+}
+
+const dice = new Roll();
+
+function handleRoll(rollText: string): string {
+  const valid = dice.validate(rollText);
+  if (!valid) {
+    return `'${rollText}' is not a valid roll!`;
+  } else {
+    const roll = dice.roll(rollText);
+    console.debug(`'${rollText}' resulted in ${roll}.`);
+    let response = `${roll.result}`;
+    if (typeof(roll.rolled) !== 'number') {
+      response += ' - ' + JSON.stringify(roll.rolled);
+    }
+    return response;
+  }
 }
